18a2636ca8d5d7b9ac4279fe3ed0e9b57802d1bb
jmpala <home@DESKTOP-NV371VC.localdomain>
2020-04-29 04:13:48 +0200

    Correction class name for IndexController - close #42

--- src/main/java/dev/jmpalazzolo/springblog/controllers/IndexController.java
+++ src/main/java/dev/jmpalazzolo/springblog/controllers/IndexController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class indexController {
+public class IndexController {
 
     @GetMapping("/")
-    String indexPage() {
+    public String indexPage() {
         return "/index";
     }
 }
